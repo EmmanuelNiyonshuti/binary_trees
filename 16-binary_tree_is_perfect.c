c11d4856b0809d77918af9ef47166e179b9a20af
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-11 18:42:34 +0200

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,25 @@
 #include "binary_trees.h"
 /**
-*binary_tree_h - gets the height of the binary tree.
+*binary_tree_h - Gets the height of the binary tree.
 *@tree: Pointer to the tree to go through.
 *
-*Return: The Height , 0 on failure.
+*Return: The height , 0 on failure.
 */
 int binary_tree_h(binary_tree_t *tree)
 {
-    int len_left, len_right;
-    
-    if (!tree)
-	return (0);
+	int len_left, len_right;
 
-    if (!tree->left && !tree->right)
-        return (0);
+	if (!tree)
+		return (0);
 
-    len_left = binary_tree_h(tree->left);
+	if (!tree->left && !tree->right)
+		return (0);
+
+	len_left = binary_tree_h(tree->left);
 	len_right = binary_tree_h(tree->right);
-    return (len_left > len_right ? len_left + 1 : len_right + 1);
+	return (len_left > len_right ? len_left + 1 : len_right + 1);
 }
-/*
+/**
 *binary_tree_f - checking if the internal nodes have 2 subtrees
 *@tree: Pointer to the tree to go through.
 *
@@ -31,6 +31,7 @@ int binary_tree_f(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+
 	l = binary_tree_f(tree->left);
 	r = binary_tree_f(tree->right);
 
@@ -46,25 +47,17 @@ int binary_tree_f(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    bool t_l, t_r;
-    int h_l, h_r;
+	bool t_l, t_r;
+	int h_l, h_r;
 
-    if (!tree)
-        return (0);
-    t_l = binary_tree_f(tree->left);
-    t_r = binary_tree_f(tree->right);
-    h_r = binary_tree_h(tree->left);
-    h_l = binary_tree_h(tree->right);
-    if (((t_l && t_r) == 1) && (h_r == h_l))
-        return (1);
-    return (0);
+	if (!tree)
+		return (0);
 
-    /*checker did not like this*/
-    /*t_l = binary_tree_is_full(tree->left);
-    t_r = binary_tree_is_full(tree->right);
-    h_r = binary_tree_height(tree->left);
-    h_l = binary_tree_height(tree->right);
-    if (((t_l && t_r) == 1) && (h_r == h_l))
-        return (1);
-    return (0);*/
+	t_l = binary_tree_f(tree->left);
+	t_r = binary_tree_f(tree->right);
+	h_r = binary_tree_h(tree->left);
+	h_l = binary_tree_h(tree->right);
+	if (((t_l && t_r) == 1) && (h_r == h_l))
+		return (1);
+	return (0);
 }
