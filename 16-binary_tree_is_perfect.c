1fdb7be6ba607092747be59605bc26ab0b55bb64
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-11 21:25:07 +0200

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,8 +32,8 @@ int binary_tree_f(const binary_tree_t *tree)
 
 	binary_tree_f(tree->left);
     binary_tree_f(tree->right);
-	return ((((tree->left && tree->right))
-			 || (!tree->left && !tree->right)) ? 1 : 0);
+	return ((((tree->left && tree->right)) || (!tree->left || !tree->right))
+                        || (!tree->left && !tree->right) ? 1 : 0);
 }
 /**
 *binary_tree_is_perfect - Checks if a binary tree is perfect.
