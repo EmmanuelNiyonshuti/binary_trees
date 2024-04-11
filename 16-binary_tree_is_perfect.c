59c978059c0dacf3e8de037b1428f4ead96a41c3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-11 17:23:49 +0200

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,30 +1,44 @@
 #include "binary_trees.h"
-
+/**
+*binary_tree_h - gets the height of the binary tree.
+*@tree: Pointer to the tree to go through.
+*
+*Return: The Height , 0 on failure.
+*/
 int binary_tree_h(binary_tree_t *tree)
 {
     int len_left, len_right;
-    
+
     if (!tree)
-	return (0);
+    return -1;
 
     if (!tree->left && !tree->right)
-        return (0);
+    return 0;
 
     len_left = binary_tree_h(tree->left);
-	len_right = binary_tree_h(tree->right);
+    len_right = binary_tree_h(tree->right);
+
     return (len_left > len_right ? len_left + 1 : len_right + 1);
 }
+/*
+*binary_tree_f - checking if the internal nodes have 2 subtrees
+*@tree: Pointer to the tree to go through.
+*
+*Return: 1 on success 0 on failure.
+*/
 int binary_tree_f(const binary_tree_t *tree)
 {
-	int r, l;
+    if (!tree)
+        return 1; // An empty tree is considered a full binary tree
 
-	if (!tree)
-		return (0);
-	l = binary_tree_f(tree->left);
-	r = binary_tree_f(tree->right);
+    if (!tree->left && !tree->right)
+        return 1; // Leaf node is considered full
 
-	return ((((r && l) && (tree->left && tree->right))
-			 || (!tree->left && !tree->right)) ? 1 : 0);
+    if (tree->left && tree->right)
+        return binary_tree_f(tree->left) && binary_tree_f(tree->right);
+
+    return 0;
+
 }
 /**
 *binary_tree_is_perfect - Checks if a binary tree is perfect.
@@ -35,16 +49,7 @@ int binary_tree_f(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    bool t_l, t_r;
-    int h_l, h_r;
-
     if (!tree)
         return (0);
-    t_l = binary_tree_f(tree->left);
-    t_r = binary_tree_f(tree->right);
-    h_r = binary_tree_h(tree->left);
-    h_l = binary_tree_h(tree->right);
-    if (((t_l && t_r) == 1) && (h_r == h_l))
-        return (1);
-    return (0);
+    return binary_tree_f(tree) && (binary_tree_h(tree->left) == binary_tree_h(tree->right));  
 }
